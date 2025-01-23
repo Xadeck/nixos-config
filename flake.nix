1a9f6317d40c3ced932f8a83769a95b31bff6553
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2025-01-23 23:08:41 +0100

    Initial import

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "GKMtec";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.gkmtec = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
