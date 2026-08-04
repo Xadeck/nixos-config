df4b09f705d72fb5b06448a5eeb548b745221345
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2026-08-04 16:24:30 +0200

    feat(flake): add pre-commit hooks via git-hooks.nix
    - Add git-hooks-nix input and configure pre-commit hook for Alejandra formatting
    - Expose devShells.default to auto-install hooks on nix develop

--- flake.nix
+++ flake.nix
@@ -7,11 +7,34 @@
       url = "github:jacopone/antigravity-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    git-hooks-nix,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    checks.${system}.pre-commit-check = git-hooks-nix.lib.${system}.run {
+      src = ./.;
+      hooks = {
+        alejandra.enable = true;
+      };
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      inherit (self.checks.${system}.pre-commit-check) shellHook;
+    };
+
     nixosConfigurations.gmktec = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
