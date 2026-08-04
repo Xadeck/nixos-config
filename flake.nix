07cd5b64a470c45dae2640effc452506ddba8827
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2026-08-04 15:58:07 +0200

    Added antigravity.

--- flake.nix
+++ flake.nix
@@ -3,17 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    antigravity-nix = {
+      url = "github:jacopone/antigravity-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    antigravity-nix,
     ...
   } @ inputs: {
     nixosConfigurations.gkmtec = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        {
+          # Allow unfree packages since Antigravity is proprietary
+          nixpkgs.config.allowUnfree = true;
+
+          environment.systemPackages = [
+            antigravity-nix.packages.x86_64-linux.default # Antigravity Base App
+            antigravity-nix.packages.x86_64-linux.google-antigravity-ide # Antigravity IDE
+            antigravity-nix.packages.x86_64-linux.google-antigravity-cli # CLI (agy)
+          ];
+        }
       ];
     };
   };
